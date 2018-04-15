dd8cdc939fb7b66cc338ddc8d9e3db73272f018d
Vogete <fraknoiz@gmail.com>
2018-04-16 00:22:42 +0200

    rawdata assingment 5: question 4

--- RAWDATA/Assignments/Assignment5/Question4.sql
+++ RAWDATA/Assignments/Assignment5/Question4.sql
@@ -7,3 +7,13 @@ SELECT DISTINCT word, pos
     FROM words
     GROUP BY word, pos
 ;
+
+SELECT *
+    FROM words w1, words w2
+    WHERE
+        w1.word = w2.word
+        AND
+        w1.pos = w2.pos
+        AND NOT
+        w1.lemma = w2.lemma
+;
